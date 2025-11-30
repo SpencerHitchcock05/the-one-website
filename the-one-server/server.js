018042fea516307ff4668267a2a62f4a43036480
Spencer <sp797499@dal.ca>
2025-11-29 23:22:44 -0400

    changed image grabbing to puppeteer stealth to scrape images

--- the-one-server/server.js
+++ the-one-server/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const cheerio = require('cheerio');
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import cheerio from 'cheerio';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
 const app = express();
 const PORT = 8000;
 
@@ -9,6 +12,21 @@ app.use(cors({
     origin: '*',
     methods: ['GET'],
 }),)
+
+puppeteer.use(StealthPlugin());
+
+async function fetchPage(url) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url, {
+    waitUntil: "networkidle2"
+    });
+
+    const html = await page.content();
+    await browser.close();
+
+    return html
+}
 
 app.get('/:name', (req, res) => {
     axios.get(`https://lotr.fandom.com/wiki/${req.params.name}`) //document.querySelector("#mw-content-text > div > p:nth-child(6)")
@@ -23,8 +41,6 @@ app.get('/:name', (req, res) => {
                 }
             }
             
-            
-
             res.json({text: texts});
         })
         .catch(() => {
@@ -33,20 +49,13 @@ app.get('/:name', (req, res) => {
     
 })
 
-app.get('/:name/img', (req, res) => {
-    axios.get(`https://tolkiengateway.net/wiki/${req.params.name}`)
-        .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            const imgPath = $(".image > img");
-            const img = imgPath[0].attribs.src;
-            res.json({img: 'https://tolkiengateway.net' + img});
-        }).catch(err => {
-            res.json({img: ''})
-        })
+app.get('/:name/img', async (req, res) => {
+    const html = await fetchPage(`https://tolkiengateway.net/wiki/${req.params.name}`);
+    const $ = cheerio.load(html);
+    const imgPath = $(".mw-file-description img");
+    const img = imgPath[0].attribs.src;
+    res.json({img: 'https://tolkiengateway.net' + img});
 })
-
-
 
 
 app.listen(PORT, () => {
